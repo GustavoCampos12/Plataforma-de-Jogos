8b86c29ff3b0ad55016a82a3284d147b407b1d6e
Gustavo Campos <gustavo@workverse.digital>
2024-09-20 19:01:12 -0300

    Commit da aplicação

---
+++ src/componentes/ItemJogos/index.js
@@ -0,0 +1,16 @@
+
+import "./style.css";
+
+const ItemJogos = ({ nome, plataforma }) => {
+  return (
+    <div className="card">
+      <div>
+
+      </div>
+      <div>
+        <p>{nome}</p>
+      </div>
+    </div>
+  );
+};
+export default ItemJogos;
